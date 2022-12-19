26ca0364e68f82fed048556fffd5d808d0f5ad70
ahnilhuman2 <ahnilch1717@gmail.com>
2022-12-19 15:40:04 +0900

    프로그래머스 0단계

---
+++ src/main/java/com/example/algorithmstudy/level0_04/solution.java
@@ -0,0 +1,10 @@
+package com.example.algorithmstudy.level0_04;
+
+// 정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+
+public class solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ src/main/java/com/example/algorithmstudy/q01해시02/solution.java
@@ -0,0 +1,4 @@
+package com.example.algorithmstudy.q01해시02;
+
+public class solution {
+}

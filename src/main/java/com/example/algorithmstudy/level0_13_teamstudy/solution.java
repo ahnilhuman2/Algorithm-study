9db540095ceabaa427a3a2407b89c910241af5ad
ahnilhuman2 <ahnilch1717@gmail.com>
2022-12-20 12:38:46 +0900

    programmers team study 3번 완료

--- src/main/java/com/example/algorithmstudy/level0_13_teamstudy/solution.java
+++ src/main/java/com/example/algorithmstudy/level0_13_teamstudy/solution.java
@@ -3,21 +3,24 @@ package com.example.algorithmstudy.level0_13_teamstudy;
 // 최빈값은 주어진 값 중에서 가장 자주 나오는 값을 의미합니다. 정수 배열 array가 매개변수로 주어질 때, 최빈값을 return 하도록 solution 함수를 완성해보세요.
 // 최빈값이 여러 개면 -1을 return 합니다.
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 
 public class solution {
     public int solution(int[] array) {
-
-        int cnt = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < array.length; i++) {
-            map.put(array[i], cnt);
-            if (array[i] == array[i+1]) {
-                map.put(array[i], cnt++);
+        int maxCnt = 0;
+        int maxNum = 0;
+        for (int a : array) {
+            map.put(a, map.getOrDefault(a, 0) + 1);
+            if (map.get(a) > maxCnt) {
+                maxCnt = map.get(a);
+                maxNum = a;
+            }else if (maxCnt == map.get(a)) {
+                maxNum = -1;
             }
         }
-        return map.get(cnt);
+        return maxNum;
     }
 }

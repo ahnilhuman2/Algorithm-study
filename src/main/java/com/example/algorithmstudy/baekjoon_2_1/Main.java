369204008a026e5867250abc80b9528ec5da5856
ahnilhuman2 <ahnilch1717@gmail.com>
2022-12-20 14:54:59 +0900

    백준 1단계

---
+++ src/main/java/com/example/algorithmstudy/baekjoon_2_1/Main.java
@@ -0,0 +1,19 @@
+package com.example.algorithmstudy.baekjoon_2_1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a > b) {
+            System.out.println(">");
+        }else if (a < b) {
+            System.out.println("<");
+        }else {
+            System.out.println("==");
+        }
+    }
+}

fce5d6c954af78bcef1e278f8031a2584dadeb7f
ahnilhuman2 <ahnilch1717@gmail.com>
2022-12-20 13:51:56 +0900

    백준 1단계 문제들

---
+++ src/main/java/com/example/algorithmstudy/baekjoon_1_8/Main.java
@@ -0,0 +1,13 @@
+package com.example.algorithmstudy.baekjoon_1_7;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        System.out.println(a + "??!");
+
+    }
+}

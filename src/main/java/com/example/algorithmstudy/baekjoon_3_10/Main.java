9343a52d5e86ea3855fc751002d769d4014d9de0
ahnilhuman2 <ahnilch1717@gmail.com>
2022-12-22 19:03:36 +0900

    백준 3단계 10번

---
+++ src/main/java/com/example/algorithmstudy/baekjoon_3_10/Main.java
@@ -0,0 +1,22 @@
+package com.example.algorithmstudy.baekjoon_3_10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int result;
+
+        while (sc.hasNext()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            result = a + b;
+            System.out.println(result);
+        }
+    }
+}

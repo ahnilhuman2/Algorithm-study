dbc46a38cfe3ba2275fbf7f1a49bd69d64cc7352
ahnilhuman2 <ahnilch1717@gmail.com>
2022-12-22 16:49:40 +0900

    백준 3단계 3번

---
+++ src/main/java/com/example/algorithmstudy/baekjoon_3_4/Main.java
@@ -0,0 +1,28 @@
+package com.example.algorithmstudy.baekjoon_3_4;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int totalPrice = sc.nextInt();
+        int variety = sc.nextInt();
+        int result = 0;
+
+        for (int i = 0; i < variety; i++) {
+            int price =  sc.nextInt();
+            int num =  sc.nextInt();
+
+            result +=  price * num;
+        }
+
+
+        if (result == totalPrice) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+
+    }
+}

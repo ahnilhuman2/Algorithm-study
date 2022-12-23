fd165817b3440bca70caaf071860660a214f5fe1
ahnilhuman2 <ahnilch1717@gmail.com>
2022-12-23 13:11:08 +0900

    백준 4단계 5번

--- src/main/java/com/example/algorithmstudy/baekjoon_4_5/Main.java
+++ src/main/java/com/example/algorithmstudy/baekjoon_4_5/Main.java
@@ -1,2 +1,22 @@
-package com.example.algorithmstudy.baekjoon_4_5;public class Main {
+package com.example.algorithmstudy.baekjoon_4_5;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean[] arr = new boolean[30];
+
+        for (int i = 0; i < arr.length-2; i++) {
+            int a = sc.nextInt();
+            arr[a - 1] = true;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!arr[i]) {
+                System.out.println(i+1);
+            }
+        }
+
+    }
 }

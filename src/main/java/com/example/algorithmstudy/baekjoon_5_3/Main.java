82ee27d80f5448c3380c426f47244b0c58ecdce5
ahnilhuman2 <ahnilch1717@gmail.com>
2022-12-26 19:34:59 +0900

    백준 6단계 1번

--- src/main/java/com/example/algorithmstudy/baekjoon_5_3/Main.java
+++ src/main/java/com/example/algorithmstudy/baekjoon_5_3/Main.java
@@ -25,7 +25,7 @@ public class Main {
                 int d = (i/10)%10;
                 int e = i%10;
 
-                if ((c - d) == (d - c)) {
+                if ((c - d) == (d - e)) {
                     cnt++;
                 }
             }
@@ -33,3 +33,33 @@ public class Main {
         return cnt;
     }
 }
+
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print(han(sc.nextInt()));
+//    }
+//
+//    public static int han(int a) {
+//        int cnt = 0; // 한수 카운팅
+//
+//        if (a < 100) {
+//            return a;
+//        }else {
+//            cnt = 99;
+//
+//            for (int i = 100; i <= a; i++) {
+//                int b = i / 100;
+//                int c = (i / 10) % 10;
+//                int d = i % 10;
+//
+//                if ((b - c) == (c - d)) {
+//                    cnt++;
+//                }
+//            }
+//        }
+//        return cnt;
+//    }
+//}

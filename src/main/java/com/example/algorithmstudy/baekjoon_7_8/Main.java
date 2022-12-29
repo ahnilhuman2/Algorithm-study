30c529b5a974de9113b140e3b71ac61d33651433
ahnilhuman2 <ahnilch1717@gmail.com>
2022-12-29 19:08:36 +0900

    백준 7단계 8번

--- src/main/java/com/example/algorithmstudy/baekjoon_7_8/Main.java
+++ src/main/java/com/example/algorithmstudy/baekjoon_7_8/Main.java
@@ -1,4 +1,17 @@
 package com.example.algorithmstudy.baekjoon_7_8;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger a = new BigInteger(sc.next());
+        BigInteger b = new BigInteger(sc.next());
+
+        a = a.add(b);
+
+        System.out.println(a);
+    }
 }

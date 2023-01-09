7b4865b761d993d51a271dcd841ea3ae2164b4da
ahnilhuman2 <ahnilch1717@gmail.com>
2023-01-09 16:22:32 +0900

    백준 12단계 2번

--- src/main/java/com/example/algorithmstudy/baekjoon_12_2/Main.java
+++ src/main/java/com/example/algorithmstudy/baekjoon_12_2/Main.java
@@ -1,2 +1,33 @@
-package com.example.algorithmstudy.baekjoon_12_2;public class Main {
+package com.example.algorithmstudy.baekjoon_12_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int answer = 0;
+
+        for (int i = 0; i < N; i++) {
+            int sum = 0;
+            int num = i;
+
+            while (num != 0) {
+                sum += num%10;
+                num /= 10;
+            }
+
+            if (N == sum + i) {
+                answer = i;
+                break;
+            }
+        }
+
+        System.out.println(answer);
+
+    }
 }

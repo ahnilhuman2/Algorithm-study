f60d2a334c6194b13dbe239a9ec4cf2c22fa1614
ahnilhuman2 <ahnilch1717@gmail.com>
2023-01-16 14:41:34 +0900

    백준 14단계 4, 5번

---
+++ src/main/java/com/example/algorithmstudy/baekjoon_14_4/Main.java
@@ -0,0 +1,36 @@
+package com.example.algorithmstudy.baekjoon_14_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int cham = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[6];
+
+        StringTokenizer st;
+
+        for (int i = 0; i < 6; i++) {
+            st = new StringTokenizer(br.readLine());
+            st.nextToken();
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int area = 0;
+        int maxArea = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int tmp = arr[i] * arr[i == 5? 0 : i + 1];
+            if (area < tmp) {
+                area = tmp;
+            }
+            maxArea += tmp;
+        }
+        System.out.println((maxArea - area*2) * cham);
+    }
+}

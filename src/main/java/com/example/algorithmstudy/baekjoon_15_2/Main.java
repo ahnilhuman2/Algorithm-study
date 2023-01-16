a1f0a6a3b45ff4bf310ef5a68b054d256b989125
ahnilhuman2 <ahnilch1717@gmail.com>
2023-01-16 17:30:18 +0900

    백준 15단계 2번

---
+++ src/main/java/com/example/algorithmstudy/baekjoon_15_2/Main.java
@@ -0,0 +1,28 @@
+package com.example.algorithmstudy.baekjoon_15_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int case1 = Integer.parseInt(br.readLine());
+        int[] arr = new int[case1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < case1; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int result = arr[0] * arr[case1 - 1];
+
+        System.out.println(result);
+    }
+}

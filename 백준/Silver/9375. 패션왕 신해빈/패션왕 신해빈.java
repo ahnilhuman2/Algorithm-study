99fdb13adb7ad9800290b3fcd5614e0e97d3b2b2
ahnilhuman2 <112387307+ahnilhuman2@users.noreply.github.com>
2023-01-19 16:39:25 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 132 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int case1 = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for (int i = 0; i < case1; i++) {
+
+            Map<String, Integer> map = new HashMap<>();
+            int case2 = Integer.parseInt(br.readLine());
+
+            for (int j = 0; j < case2; j++) {
+
+                st = new StringTokenizer(br.readLine());
+                st.nextToken();
+                String type =  st.nextToken();
+
+                map.put(type, map.getOrDefault(type, 0) + 1);
+            }
+
+            int res = 1;
+
+            for (int a : map.values()) {
+                res *= (a + 1);
+            }
+            
+            sb.append(res - 1).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}

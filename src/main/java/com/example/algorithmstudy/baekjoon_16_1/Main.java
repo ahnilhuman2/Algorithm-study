37f85f952e6fc67a943909b0524026980d3a2d43
ahnilhuman2 <ahnilch1717@gmail.com>
2023-01-20 15:49:24 +0900

    백준 16단계 1번

---
+++ src/main/java/com/example/algorithmstudy/baekjoon_16_1/Main.java
@@ -0,0 +1,45 @@
+package com.example.algorithmstudy.baekjoon_16_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int[] arr;
+    public static boolean[] visit;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        visit = new boolean[N + 1];
+        dfs(N, M, 0);
+        System.out.println(sb);
+
+    }
+
+    public static void dfs(int N, int M, int depth) {
+        if (M == depth) {
+            for (int n : arr) {
+                sb.append(n).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i;
+                dfs(N, M, depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}

0fe40d7317d767dff044442496fbdbcf703df2de
ahnilhuman2 <ahnilch1717@gmail.com>
2023-01-31 15:44:09 +0900

    카카오 1단계

--- src/main/java/com/example/algorithmstudy/programmers_150370/Solution.java
+++ src/main/java/com/example/algorithmstudy/programmers_150370/Solution.java
@@ -1,30 +1,55 @@
-package com.example.algorithmstudy.programmers_72410;
+package com.example.algorithmstudy.programmers_150370;
 
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
-    public String solution(String new_id) {
+    public int[] solution(String today, String[] terms, String[] privacies) {
 
-        new_id = new_id.toLowerCase();
+        String[] tmp = today.split("\\.");
+        int year = Integer.parseInt(tmp[0]);
+        int month = Integer.parseInt(tmp[1]);
+        int day = Integer.parseInt(tmp[2]);
+        int td = (year * 12 * 28) + (month * 28) + day;
 
-        new_id = new_id.replaceAll("[^a-z0-9-_.]","");
+        Map<String, Integer> map = new HashMap<>();
 
-        new_id = new_id.replaceAll("\\.{2,}",".");
 
-        new_id = new_id.replaceAll("^(\\.)|(\\.)$","");
-
-        if (new_id.isEmpty()) {
-            new_id = "a";
+        for (int i = 0; i < terms.length; i++) {
+            String[] str = terms[i].split(" ");
+            map.put(str[0], Integer.valueOf(str[1]));
         }
 
-        if (new_id.length() >= 16) {
-            new_id = new_id.substring(0, 15).replaceAll("(\\.)$", "");
+        String[] arr1 = new String[privacies.length];
+        String[] arr2 = new String[privacies.length];
+
+        for (int i = 0; i < privacies.length; i++) {
+            String[] arr =  privacies[i].split(" ");
+            arr1[i] = arr[0];
+            arr2[i] = arr[1];
         }
 
-        while (new_id.length() < 3) {
-            new_id += new_id.charAt(new_id.length() - 1);
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < arr1.length; i++) {
+            String[] arr = arr1[i].split("\\.");
+            int tYear = Integer.parseInt(arr[0]);
+            int tMonth = Integer.parseInt(arr[1]);
+            int tDay = Integer.parseInt(arr[2]);
+            int tTd = (tYear * 12 * 28) + (tMonth * 28) + tDay;
+
+
+            if (td >= tTd + map.get(arr2[i]) * 28) {
+                list.add(i + 1);
+            }
         }
 
-        return new_id;
+        int[] answer = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
     }
 }

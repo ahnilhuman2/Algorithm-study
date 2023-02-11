032513ae81b4e4e496f3bca40f1287575d4345c0
ahnilhuman2 <ahnilch1717@gmail.com>
2023-02-11 17:43:26 +0900

    백준 17단계 9번

---
+++ src/main/java/com/example/algorithmstudy/baekjoon_17_9/Main.java
@@ -0,0 +1,24 @@
+package com.example.algorithmstudy.baekjoon_17_9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        sb.append(isRight(N, 0));
+        System.out.println(sb);
+    }
+
+    static int isRight(int N, int cnt) {
+
+        if (N < 2) {
+            return cnt;
+        }
+        return Math.min(isRight(N / 2, cnt + 1 + (N % 2)), isRight(N / 3, cnt + 1 + (N % 3)));
+    }
+}

3810546e26f0afaf1c7dcc446e6aaa78a4ed3263
ahnilhuman2 <ahnilch1717@gmail.com>
2023-04-14 15:28:02 +0900

    update

--- src/main/java/com/example/algorithmstudy/programmers_42579/Solution.java
+++ src/main/java/com/example/algorithmstudy/programmers_42579/Solution.java
@@ -52,11 +52,6 @@ public class Solution {
             }
         }
 
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-
-        return answer;
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }

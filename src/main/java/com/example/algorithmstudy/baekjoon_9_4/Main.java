93d0e3740caff26bc889318d77476e373e008388
ahnilhuman2 <ahnilch1717@gmail.com>
2023-04-16 17:40:19 +0900

    update

---
+++ src/main/java/com/example/algorithmstudy/baekjoon_9_4/Main.java
@@ -0,0 +1,33 @@
+package com.example.algorithmstudy.baekjoon_9_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        while (true) {
+            StringBuilder sb = new StringBuilder();
+            int n = Integer.parseInt(br.readLine());
+            if (n == -1) break;
+
+            int sum = 0;
+            for (int i = 1; i < n; i++) {
+                if (n % i == 0) {
+                    sb.append(i).append(" + ");
+                    sum += i;
+                }
+            }
+
+            if (n == sum) {
+                System.out.println(n + " = " + sb.toString().substring(0, sb.length() - 3));
+            } else {
+                System.out.println(n + " is NOT perfect.");
+            }
+        }
+    }
+}

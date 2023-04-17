7546e08e4e42c15bfd1a515fc954eafb3e8a165b
ahnilhuman2 <ahnilch1717@gmail.com>
2023-04-17 14:49:01 +0900

    update

---
+++ src/main/java/com/example/algorithmstudy/programmers_42587/Solution.java
@@ -0,0 +1,27 @@
+package com.example.algorithmstudy.programmers_42587;
+
+import java.util.*;
+
+public class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int priority : priorities) {
+            pq.offer(priority);
+        }
+
+        while (pq.size() != 0) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == pq.peek()) {
+                    pq.poll();
+                    answer++;
+                    if (i == location) {
+                        return answer;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}

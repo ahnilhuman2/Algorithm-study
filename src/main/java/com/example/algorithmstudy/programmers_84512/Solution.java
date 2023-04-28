84138118eb12f324b27124ba88a26c9399ccc29a
ahnilhuman2 <ahnilch1717@gmail.com>
2023-04-28 16:10:19 +0900

    update

--- src/main/java/com/example/algorithmstudy/programmers_84512/Solution.java
+++ src/main/java/com/example/algorithmstudy/programmers_84512/Solution.java
@@ -1,11 +1,30 @@
 package com.example.algorithmstudy.programmers_84512;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Solution {
+
+    static String[] arr = {"A", "E", "I", "O", "U"};
+    static int cnt;
+    static int answer;
+
     public int solution(String word) {
-        int answer = 0;
+        dfs("", word);
         return answer;
+    }
+
+    public void dfs(String str, String word) {
+        if (str.length() == 5) {
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            String tmp = str + arr[i];
+            cnt++;
+
+            if (tmp.equals(word)) {
+                answer = cnt;
+                return;
+            }
+            dfs(tmp, word);
+        }
     }
 }

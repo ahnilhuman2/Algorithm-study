6e7e958c1b8460315a48a550f55e53c200aa822b
ahnilhuman2 <112387307+ahnilhuman2@users.noreply.github.com>
2023-05-08 15:11:44 +0900

    [Silver IV] Title: 베스트셀러, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int case1 = Integer.parseInt(br.readLine());
+        HashMap<String, Integer> map = new HashMap<>();
+        String answer = null;
+
+        for (int i = 0; i < case1; i++) {
+            String book = br.readLine();
+            map.put(book, map.getOrDefault(book, 0) + 1);
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        for (String str : map.keySet()) {
+            list.add(str);
+        }
+        Collections.sort(list);
+        int max = 0;
+
+        for (String str : list) {
+            if (map.get(str) > max) {
+                max = map.get(str);
+                answer = str;
+            }
+        }
+        sb.append(answer);
+        System.out.println(sb);
+    }
+}

77f8505300d5350b159adc7e7c4d57cc6d8f29f3
ahnilhuman2 <112387307+ahnilhuman2@users.noreply.github.com>
2023-05-12 14:57:59 +0900

    [Silver III] Title: 파일 정리, Time: 928 ms, Memory: 68208 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            String[] str = br.readLine().split("\\.");
+            String type = str[1];
+            map.put(type, map.getOrDefault(type, 0) + 1);
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        for (String key : map.keySet()) {
+            list.add(key);
+        }
+        Collections.sort(list);
+
+        for (String key : list) {
+            sb.append(key).append(" ").append(map.get(key)).append("\n");
+        }
+        System.out.println(sb);
+    }
+}

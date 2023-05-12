2b914820f47b697b8656d3f705f2e9b95888ae92
ahnilhuman2 <112387307+ahnilhuman2@users.noreply.github.com>
2023-05-12 14:40:14 +0900

    [Silver IV] Title: 차집합, Time: 1384 ms, Memory: 158168 KB -BaekjoonHub

---
+++ 백준/Silver/1822. 차집합/차집합.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            map.put(a, 1);
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int b = Integer.parseInt(st.nextToken());
+            map.remove(b);
+        }
+        sb.append(map.size()).append('\n');
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int n : map.keySet()) {
+            list.add(n);
+        }
+        Collections.sort(list);
+
+        for (int n : list) {
+            sb.append(n).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
+

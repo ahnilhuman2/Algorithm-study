14e40ea398c9789369dc93dad06167933b7b290b
ahnilhuman2 <112387307+ahnilhuman2@users.noreply.github.com>
2023-05-15 14:39:02 +0900

    [Bronze I] Title: 민균이의 비밀번호, Time: 124 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/9933. 민균이의 비밀번호/민균이의 비밀번호.java
+++ 백준/Bronze/9933. 민균이의 비밀번호/민균이의 비밀번호.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,28 +9,18 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         int case1 = Integer.parseInt(br.readLine());
-        ArrayList<String> list = new ArrayList<>();
+        HashSet<String> set = new HashSet<>();
         for (int i = 0; i < case1; i++) {
-            list.add(br.readLine());
+            set.add(br.readLine());
         }
 
-        String[] arr = new String[case1];
-        String[] arrRe = new String[case1];
-        for (int i = 0; i < case1; i++) {
-            arr[i] = list.get(i);
-        }
-        for (int i = 0; i < case1; i++) {
-            arrRe[i] = new StringBuilder(arr[i]).reverse().toString();
-        }
-
-        for (int i = 0; i < case1; i++) {
-            for (int j = 0; j < case1; j++) {
-                if (arr[i].equals(arrRe[j])) {
-                    sb.append(arr[i].length()).append(" ").append(arr[i].charAt(arr[i].length() / 2));
-                    System.out.println(sb);
-                    return;
-                }
+        for (String str : set) {
+            String str2 = new StringBuilder(str).reverse().toString();
+            if (set.contains(str2)) {
+                sb.append(str.length()).append(" ").append(str.charAt(str.length() / 2));
+                break;
             }
         }
+        System.out.println(sb);
     }
 }

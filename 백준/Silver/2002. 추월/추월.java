8f8b14d2e96e66daced75049e32e3325d39f7a14
ahnilhuman2 <112387307+ahnilhuman2@users.noreply.github.com>
2023-05-15 15:22:52 +0900

    [Silver I] Title: 추월, Time: 128 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Silver/2002. 추월/추월.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        HashMap<String, Integer> map = new HashMap<>();
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+           map.put(br.readLine(), i);
+        }
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            arr[i] = map.get(str);
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (arr[i] > arr[j]) {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+        sb.append(cnt);
+        System.out.println(sb);
+    }
+}

d199a2d7df8ec13e353cb1c0b06a3725725ceb28
ahnilhuman2 <ahnilch1717@gmail.com>
2023-05-15 14:58:19 +0900

    update

---
+++ src/main/java/com/example/algorithmstudy/baekjoon_13414/Main.java
@@ -0,0 +1,36 @@
+package com.example.algorithmstudy.baekjoon_13414;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int K = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        LinkedHashSet<String> set = new LinkedHashSet<>();
+        for (int i = 0; i < L; i++) {
+            String str = br.readLine();
+            if (set.contains(str)) {
+                set.remove(str);
+            }
+            set.add(str);
+        }
+
+        for (String str : set) {
+            sb.append(str).append("\n");
+            K--;
+            if (K == 0) {
+                break;
+            }
+        }
+        System.out.println(sb);
+    }
+}

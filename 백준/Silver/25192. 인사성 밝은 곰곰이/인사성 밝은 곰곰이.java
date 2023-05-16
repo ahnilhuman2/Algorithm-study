c9c6c9eef96583aea65aab91f840658e06b4271b
ahnilhuman2 <112387307+ahnilhuman2@users.noreply.github.com>
2023-05-16 15:47:25 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 308 ms, Memory: 27620 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        HashMap<String, Integer> map = new HashMap<>();
+
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            if ("ENTER".equals(str)) {
+                map = new HashMap<>();
+                continue;
+            }
+            map.put(str, map.getOrDefault(str, 0) + 1);
+
+            if (map.get(str) == 1) {
+                cnt++;
+            }
+        }
+        sb.append(cnt);
+        System.out.println(sb);
+    }
+}
